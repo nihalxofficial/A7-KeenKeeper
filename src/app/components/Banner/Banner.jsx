b2c350d2d95e45241d1b5ce06fdf73f5d57bbbaa
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-04-16 22:01:40 +0600

    Banner section added

--- src/app/components/Banner/Banner.jsx
+++ src/app/components/Banner/Banner.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { FaPlus } from 'react-icons/fa';
 
 const Banner = () => {
     return (
-        <div>
+        <div className='bg-[#f8fafc] pt-20 pb-10 text-center'>
+            <h2 className='font-bold text-4xl mb-4'>Friends to keep close in your life</h2>
+            <p className='text-sm text-gray-600 mb-8'>Your personal shelf of meaningful connections. Browse, tend, and nurture the <br />
+                relationships that matter most.</p>
+            <div className='flex justify-center items-center'>
+
+                <button className='btn  text-white bg-[#244d3f] flex justify-center items-center gap-1'><FaPlus />Add a Friend</button>
+            </div>
             
         </div>
     );
--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -1,5 +1,3 @@
-
-import Link from "next/link";
 import { FaHome } from "react-icons/fa";
 import { ImStatsDots } from "react-icons/im";
 import { IoMdTimer } from "react-icons/io";
@@ -9,7 +7,7 @@ const Navbar = () => {
     
     
     return (
-        <nav className="bg-white shadow-sm py-7">
+        <nav className="bg-white shadow border-b border-gray-200 py-7">
             <div className="w-10/12 mx-auto flex justify-between items-center gap-4">
                 <h2 className="font-bold text-xl md:text-2xl">Keen<span className="text-[#244d3f]">Keeper</span></h2>
                 <ul className="flex justify-between items-center gap-5 text-sm md:text-md text-[#64748B]">
