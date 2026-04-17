efdeee0a8789365ff5eadbb55ee4cf3a1eb4446f
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-04-17 18:02:50 +0600

    Select filter functionality added

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -1,23 +1,29 @@
 "use client"
-import { TiTick } from "react-icons/ti";
 import TimelineCard from "../components/TimelineCard/TimelineCard";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { InteractionContext } from "../context/interactionContext";
 
 const TimelinePage = () => {
     const {interactions} = useContext(InteractionContext)
+    const [selectedType, setSelectedType] = useState("")
+    // console.log(selectedType);
+    const filteredInteractions = selectedType ?
+     interactions.filter(itr => itr.protocol === selectedType) 
+     : interactions
+    
+
     return (
         <div className="bg-[#f8fafc] py-20">
             <div className="w-9/12 mx-auto space-y-5">
                 <h2 className="font-bold text-4xl">Timeline</h2>
-                <select defaultValue="Filter timeline" className="select">
+                <select onChange={(e)=>setSelectedType(e.target.value)} defaultValue="Filter timeline" className="select">
                     <option disabled={true} >Filter timeline</option>
                     <option value="text">Text</option>
                     <option value="call">Call</option>
                     <option value="video">Video</option>
                 </select>
                 {interactions.length <=0 ? <div className="h-50 p-10 bg-white rounded-md shadow-sm flex justify-center items-center"><h2 className="font-semibold text-xl">No Interactions Yet...</h2></div>
-                : interactions.map((interact, idx)=> <TimelineCard key={idx} interact={interact}></TimelineCard>)}
+                : filteredInteractions.map((interact, idx)=> <TimelineCard key={idx} interact={interact}></TimelineCard>)}
 
             </div>
             
